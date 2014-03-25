4f4e69fff5c6ee8586dc290a106ebf251c072475
Jim Finnis <white@pale.org>
2014-03-25 15:22:13 +0000

    oops - added cycle files

---
+++ lib/cycle.cpp
@@ -0,0 +1,164 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <stdint.h>
+
+#include "angort.h"
+#include "cycle.h"
+
+CycleDetector *CycleDetector::instance = NULL;
+
+
+/** A description of the algorithm: 
+ * - For each container object, set gc_refs equal to the object's reference count.
+ * - For each container object, find which container objects it references and decrement the referenced container's gc_refs field.
+ * - All container objects that now have a gc_refs field greater than one are referenced from outside the set of container objects. We cannot free these objects so we move them to a different set.
+ * - Any objects referenced from the objects moved also cannot be freed. We move them and all the objects reachable from them too.
+ * - Objects left in our original set are referenced only by objects within that set (ie. they are inaccessible from Python and are garbage). We can now go about freeing these objects.
+ */
+void CycleDetector::detect(){
+    GarbageCollected *p,*q;
+    
+    newlist.reset(); // clean the new list
+    
+    for(p=mainlist.head();p;p=mainlist.next(p)){
+        dfprintf("List ent : %p (next %p)\n",p,mainlist.next(p));
+        p->gc_refs = p->refct;
+    }
+    
+    // for each item, decrement the gc_refs of any items I point to.
+    // You'll notice three calls doing the work - two are general purpose, assuming
+    // the item can create value and key iterators. The other is for each object to extend,
+    // and is used when a user subclass of GarbageCollected has non-Lana properties
+    // which refer to GCable entities.
+    
+    for(p=mainlist.head();p;p=mainlist.next(p)) {
+        decIteratorReferentsCycleRefCounts(p,true);
+        decIteratorReferentsCycleRefCounts(p,false);
+        p->decReferentsCycleRefCounts();
+    }
+        
+    // now look for containers with gc_refs of greater than zero, and move them into a different list
+    
+    for(p=mainlist.head();p;p=q){
+        q=mainlist.next(p); // so we can remove during traversal
+        dfprintf("Refct : %p %d\n",p,p->gc_refs); 
+        if(p->gc_refs>=1){
+            dfprintf(" refct moving %p into new list\n",p);
+            p->gc_refs=0; // this is the initial move, we need to do this to make sure move() will work!
+            move(p);
+        }
+    }
+    
+    // only items with a gc_refs of zero will remain.
+    
+/*    
+    dfprintf("Mainlist:\n");
+    for(p=mainlist.head();p;p=mainlist.next(p))
+        dfprintf("   %p\n",p);
+    dfprintf("Newlist:\n");
+    for(p=newlist.head();p;p=newlist.next(p))
+        dfprintf("   %p\n",p);
+*/    
+    // trace objects which can be accessed from the objects we just moved, and move them too.
+    // as we do this, we set gc_refs 1 to indicate it's been done. We would do this in the step
+    // above, but we need the whole set's gc_refs to be zero so we can set it to 1 when things are
+    // processed. We also make sure we don't move objects which already have a non-zero gc_refs
+    // (move() does this).
+    // Again, as well as the general-purpose calls which run through the value/key iterators,
+    // we have a non-iterator version which is usually empty but can be overridden.
+    
+    for(p=newlist.head();p;p=q){
+        q=newlist.next(p);        
+        dfprintf("Entry : %p  - next %p\n",p,q);
+        dfprintf("moving refs from %p into new list\n",p);
+	traceAndMoveIterator(p,true);
+	traceAndMoveIterator(p,false);
+	p->traceAndMove(this);
+    }
+    dfprintf("End of loop.\n");
+    
+    // now we iterate through the list and set all reference counts of
+    // the objects we're about to delete to max.
+    
+    for(p=mainlist.head();p;p=mainlist.next(p)){
+        dfprintf("maxreffing %p\n",p);
+        p->gc_refs=0xffff;
+    }
+    
+    // then we do it again, and tell each of these objects to clear, without dereferencing,
+    // all references to the objects we just marked - this is so that we don't delete them twice.
+    // Again, as well as the general-purpose calls which run through the value/key iterators,
+    // we have a non-iterator version which is usually empty but can be overridden.
+    
+    for(p=mainlist.head();p;p=mainlist.next(p)){
+        clearZombieReferencesIterator(p,true);
+        clearZombieReferencesIterator(p,false);
+        p->clearZombieReferences();
+    }
+    
+    for(p=mainlist.head();p;p=q){
+        q=mainlist.next(p);
+        dfprintf("%p is in a cycle  - deleting\n",p);
+        delete p;
+    }
+    
+    // and we set the main list to the new list
+    
+    mainlist.copy(newlist);
+    
+//    printf("Objects left:\n");
+//    for(p=mainlist.head();p;p=mainlist.next(p)){
+//        printf("  %p (ref %d)\n",(uint32_t)p,p->refct);
+//    }
+//    printf("DONE\n");
+}
+
+void CycleDetector::decIteratorReferentsCycleRefCounts(GarbageCollected *gc, bool keys) {
+    
+    Iterator<Value *>* iterator = keys ? gc->createKeyIterator(true) : gc->createValueIterator();
+    if(!iterator)return;
+    
+    for(iterator->first();!iterator->isDone();iterator->next()){
+        Value *v = iterator->current();
+        if(GarbageCollected *gc = v->t->getGC(v)){
+            gc->gc_refs--;
+        }
+    }
+    delete iterator;
+}
+
+void CycleDetector::traceAndMoveIterator(GarbageCollected *gc, bool keys) {
+    
+    Iterator<Value *>* iterator = keys ? gc->createKeyIterator(true) : gc->createValueIterator();
+    if(!iterator)return;
+    
+    for(iterator->first();!iterator->isDone();iterator->next()){
+        Value *v = iterator->current();
+        if(GarbageCollected *g = v->t->getGC(v)){
+            if(!g->gc_refs) { // if child not done
+                move(g);
+                traceAndMoveIterator(v->v.gc,keys);
+                v->v.gc->traceAndMove(this);
+            }
+        }
+    }
+    delete iterator;
+}
+
+void CycleDetector::clearZombieReferencesIterator(GarbageCollected *gc, bool keys) {
+    Iterator<Value *>* iterator = keys ? gc->createKeyIterator(true) : gc->createValueIterator();
+    if(!iterator)return;
+    
+    for(iterator->first();!iterator->isDone();iterator->next()){
+        Value *v = iterator->current();
+        if(GarbageCollected *g = v->t->getGC(v)){
+            if(g->gc_refs == 0xffff) // if child not done
+                v->init(); // clear without any reference count changes
+        }
+    }
+    delete iterator;
+}
+
+
---
+++ lib/cycle.h
@@ -0,0 +1,219 @@
+/** @file
+ * This file contains the cycle manager, which itself contains the 
+ * global container list, which contains
+ * all entities present in the system which can refer to other
+ * objects. This is used during the occasional garbage collect to detect
+ * cycles of reference not referenced from outside. These entities
+ * are deleted.
+ */
+
+#ifndef __CYCLE_H
+#define __CYCLE_H
+
+/// doubly-linked list of GarbageCollected objects
+
+class GCList {
+    friend class GarbageCollected;
+public:
+    
+    GCList(){
+        reset();
+    }
+    
+    /// empty the list
+    
+    void reset()
+    {
+        ct = 0;
+        hd = tl = NULL;
+    }
+    
+    
+    /// return the number of entries in the list
+    
+    int entries() { return ct; }
+    
+    /// don't use this in live code, it's slow as thing.
+    bool isInList(GarbageCollected *o){
+        GarbageCollected *p;
+        for(p=hd;p;p=p->next){
+            if(p==o)return true;
+        }
+        return false;
+    }
+    
+    /// add an item to the start of the list
+    void addToHead(GarbageCollected *o)
+    {
+#ifdef DEBUG
+        if(isInList(o))
+            throw Exception("item added twice to GC list");
+#endif
+        ct++;
+        if(hd)
+        {
+            hd->prev = o;
+            o->prev = NULL;
+            o->next = hd;
+            hd = o;
+        }
+        else
+        {
+            hd = tl = o;
+            o->next = o->prev = NULL;
+        }
+    }
+    
+    /// add an item to the end of the list
+    void addToTail(GarbageCollected *o)
+    {
+#if 1
+        if(isInList(o))
+            throw Exception("item added twice to GC list");
+#endif
+        ct++;
+        if(hd)
+        {
+            tl->next = o;
+            o->next = NULL;
+            o->prev = tl;
+            tl = o;
+        }
+        else
+        {
+            hd = tl = o;
+            o->next = o->prev = NULL;
+        }
+    }
+    
+    /// remove an item from the list
+    void remove(GarbageCollected *p)
+    {
+        ct--;
+        if(p->prev)
+            p->prev->next = p->next;
+        else
+            hd = p->next;
+        if(p->next)
+            p->next->prev = p->prev;
+        else
+            tl = p->prev;
+    }
+    
+    
+    /// return the head of the list
+    GarbageCollected *head() { return hd; }
+    
+    /// return the tail of the list
+    GarbageCollected *tail() { return tl; }
+    
+    /// get next item of a given item
+    GarbageCollected *next(GarbageCollected *p) {
+        return p->next;
+    }
+    
+    /// get prev item of a given item
+    GarbageCollected *prev(GarbageCollected *p) {
+        return p->prev;
+    }
+    
+    /// copy a list from another
+    void copy(GCList &list)
+    {
+        hd = list.hd;
+        tl = list.tl;
+        ct = list.ct;
+    }
+    
+protected:
+    GarbageCollected *hd;	//!< the head of the list
+    GarbageCollected *tl; //!< the tail of the list
+    int ct;	//!< the number of entries in the list
+};
+
+/// this class contains a list of all the container objects - objects
+/// which can contain references to other objects. It can detect
+/// reference cycles within the objects in the list and destroy them.
+/// 
+/// The algorithm used is described in http://arctrix.com/nas/python/gc/
+///
+
+class CycleDetector {
+    /// initialise the cycle detector, clearing the list.
+    CycleDetector(){
+        mainlist.reset();
+    }
+    static CycleDetector *instance;
+public:
+    
+    /// singleton instance fetcher
+    static CycleDetector *getInstance(){
+        if(!instance)
+            instance = new CycleDetector();
+        return instance;
+    }
+    
+    
+    /// add an item to the cycle detector's list. This will
+    /// be any item which can hold a reference to another item.
+    
+    void add(GarbageCollected *o){
+//        printf("ADDING %p\n",o);
+        mainlist.addToTail(o);
+    }
+    
+    /// remove an item from the cycle detector's list, called when
+    /// the item is destroyed.
+    
+    void remove(GarbageCollected *o){
+//        printf("REMOVING %p\n",o);
+        mainlist.remove(o);
+    }
+    
+    /// actually detect cycles and delete objects locked in a cycle
+    /// which are not referred to from elsewhere.
+    void detect();
+    
+    /// return the number of containers in the system
+    int count(){
+        return mainlist.entries();
+    }
+    
+    /// move an item from the mainlist to the newlist
+    void move(GarbageCollected *gc) {
+        if(gc->gc_refs==0){
+            dfprintf("    MOVE %p into new list\n",gc);
+            mainlist.remove(gc);
+            newlist.addToTail(gc);
+            gc->gc_refs=1;
+        }
+    }
+    
+    /// move the entity, and the items referenced by it, into the newlist if appropriate
+    void traceAndMoveEntity(GarbageCollected *p){
+      move(p);
+      traceAndMoveIterator(p,true);
+      traceAndMoveIterator(p,false);
+      p->traceAndMove(this);
+    }
+
+private:
+    /// the list of items
+    GCList mainlist;
+    /// the list of items we build in the process of GC - becomes the new main list
+    GCList newlist;
+    
+    
+    /// decrements the reference counts of all objects referred to in the iterable
+    void decIteratorReferentsCycleRefCounts(GarbageCollected *gc,bool keys);
+    /// trace all collectable entities reachable from this iterable
+    /// If their gc_refs is zero (i.e. still in the mainlist) move
+    /// to the newlist
+    void traceAndMoveIterator(GarbageCollected *gc,bool keys);
+    
+    /// deletion prepwork - clears all references to objects marked - see detect()
+    void clearZombieReferencesIterator(GarbageCollected *gc,bool keys);
+};
+    
+
+#endif /* __CYCLE_H */
