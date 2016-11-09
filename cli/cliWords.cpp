38a8a742d9b8f7e08b97d36c188d262d27cac737
Jim Finnis <jim.finnis@gmail.com>
2016-11-09 14:31:22 +0000

    cliWord doc

--- cli/cliWords.cpp
+++ cli/cliWords.cpp
@@ -19,6 +19,8 @@ Sets a function to be called to generate the prompt. This is a
 function which has the form (gc stkcount promptchar -- string),
 so the default prompt would be:
 (|g,s,c:| ?g "|" + ?s + " " + ?c + " " +) 
+Note that any ANSI escape sequences should be wrapped with characters
+001 and 002.
 {
     promptCallback.copy(p0);
 }
