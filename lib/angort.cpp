d6eb16f1e91622f002590b2c2e66f2bc2e7ab110
Jim Finnis (on Hecate) <jim.finnis@gmail.com>
2017-02-16 16:25:21 +0000

    "round" added, version bump

--- lib/angort.cpp
+++ lib/angort.cpp
@@ -11,7 +11,7 @@
 //                      (incs on backcompat retaining features).
 //                      (incs on bug fixing patches)
 
-#define ANGORT_VERSION "2.7.2"
+#define ANGORT_VERSION "2.8.0"
 
 #include <stdlib.h>
 #include <sys/types.h>
