9de8d4e6415b3e101be134c8bd817a1eaa7d24c3
Jim Finnis (on Hecate) <jim.finnis@gmail.com>
2017-11-10 12:20:51 +0000

    Correction of spec string for future$slice

--- lib/future.cpp
+++ lib/future.cpp
@@ -14,11 +14,14 @@ namespace angort {
 
 %name future
 
-%wordargs slice vii (iterable start len -- iterable) produce a slice of a string or list
+%wordargs slice vii (iterable start end -- iterable) produce a slice of a string or list
 Return a slice of a string or list, returning another string or or list,
-given the start and length. Inappropriate types will throw ex$notcoll.
-If length of the iterable  is greater than the length requested, then the
-slice will go to the end. If the start<0 it is counted from the end.
+given the start and end positions. The end index is exclusive; that element
+will not be included. Inappropriate types will throw ex$notcoll.
+If length of the iterable is greater than the length required, then the
+output will be truncated to the remainder of the iterable.
+Negative indices count from just past the end, so -1 is the last
+element. A zero end index thus means the end.
 Empty results will be returned if the requested slice does not intersect
 the iterable.
 {
