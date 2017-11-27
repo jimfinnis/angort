6821ab8bdbecf72d0fd2a4165675b1fc6637834a
Jim Finnis <jim.finnis@gmail.com>
2017-11-27 17:31:49 +0000

    version

--- lib/angort.cpp
+++ lib/angort.cpp
@@ -11,7 +11,7 @@
 //                      (incs on backcompat retaining features).
 //                      (incs on bug fixing patches)
 
-#define ANGORT_VERSION "4.4.0"
+#define ANGORT_VERSION "4.4.1"
 
 #include <stdlib.h>
 #include <sys/types.h>
