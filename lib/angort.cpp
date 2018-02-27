19f95edae9b8431436da3ba347fa0eccfee95dea
Jim Finnis <jim.finnis@gmail.com>
2018-02-27 12:29:31 +0000

    long-delayed version bump

--- lib/angort.cpp
+++ lib/angort.cpp
@@ -11,7 +11,7 @@
 //                      (incs on backcompat retaining features).
 //                      (incs on bug fixing patches)
 
-#define ANGORT_VERSION "4.5.0"
+#define ANGORT_VERSION "4.6.0"
 
 #include <stdlib.h>
 #include <sys/types.h>
